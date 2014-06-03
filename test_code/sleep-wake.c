10445440e76b2b72ea3b94bb5203fa5ae712b618
sankarp-s06 <sankarp@cs.wisc.edu>
2014-06-02 19:54:43 -0500

    sleep-wake-timer

--- test_code/sleep-wake.c
+++ test_code/sleep-wake.c
@@ -8,16 +8,20 @@ int main(int argc, char **argv)
 	int i, pid;
 	int sum = 0;
 	int sleeptime = 1;
+	int iter = ITER;
 
 	if(argc > 1)
 		sleeptime = atoi(argv[1]);
+
+	if(argc > 2)
+		iter = atoi(argv[2]);
 
 	pid = getpid();
 
 	for(;;)
 	{
 		printf("%d runs on cpu %d\n", pid, last_cpu_scheduled());
-		for(i = 0;i < ITER; i++)
+		for(i = 0;i < iter; i++)
 			sum += i; 
 		//printf("sum %d\n", sum);
 		sleep(sleeptime); 
