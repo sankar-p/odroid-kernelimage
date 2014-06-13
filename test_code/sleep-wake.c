f39ea2bdb32177d971c9c88b8a9c4becd987f213
sankarp-s06 <sankarp@cs.wisc.edu>
2014-06-13 18:13:00 -0500

    sleep-wake-as-util

--- test_code/sleep-wake.c
+++ test_code/sleep-wake.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include "last_cpu.h"
+#include "time-measure.h"
 
 #define ITER 999999
-
+#define HITER 10
 int main(int argc, char **argv)
 {
-	int i, pid;
+	int i, j, pid;
 	int sum = 0;
 	int sleeptime = 1;
 	int iter = ITER;
+	int util = 50;
+	struct timeval start, end;
 
 	if(argc > 1)
-		sleeptime = atoi(argv[1]);
-
-	if(argc > 2)
-		iter = atoi(argv[2]);
+		util = atoi(argv[2]);
 
 	pid = getpid();
 
 	for(;;)
 	{
 		printf("%d runs on cpu %d\n", pid, last_cpu_scheduled());
-		for(i = 0;i < iter; i++)
-			sum += i; 
+		gettimeofday(&start, NULL);
+		for(j = 0;j < HITER; j++)
+			for(i = 0;i < iter; i++)
+				sum += i; 
+		gettimeofday(&start, NULL);
+
 		//printf("sum %d\n", sum);
-		sleep(sleeptime); 
+		usleep(timediff(end, start)); 
 		sum = 0;
 	}
 	return 0;
